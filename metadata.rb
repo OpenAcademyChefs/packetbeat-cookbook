ca3fa5b0c0c1064493ffbda03300acb9137cd85a
uyiqgyy <uyiqgyy@qq.com>
2015-03-28 12:25:24 +0800

    Add files.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'packetbeat-cookbook'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures packetbeat-cookbook'
+long_description 'Installs/Configures packetbeat-cookbook'
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: packetbeat-cookbook
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
